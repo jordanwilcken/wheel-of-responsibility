c8b249633624c7f9a7341860b21484a6fb7ff9c9
jordanwilcken <jordanwilcken@gmail.com>
2017-10-30 07:23:11 -0600

    more rotation code

--- elm-src/JobWheel.elm
+++ elm-src/JobWheel.elm
@@ -7,68 +7,66 @@ doStuff () =
   "done"
 
 
-getJobs : Time.Time -> JobWheel -> List Person
-
-rotateJobsOnce : Direction -> JobWheel -> JobWheel
-rotateJobsOnce direction (JobWheel record) =
-  let
-    firstToLast : List a -> List a
-    firstToLast someList =
-        case List.head someList of
-            Just item ->
-                someList
-                  |> (\list -> List.append list [ item ])
-                  |> List.drop 1
-            
-            Nothing ->
-                someList
-
-    getLast : List a -> Maybe a
-    getLast someList =
-        someList
-          |> List.reverse
-          |> List.head
-
-    dropLast : List a -> List a
-    dropLast someList =
-      let
-          numberToTake = (List.length someList) - 1
-      in
-          List.take numberToTake someList
-
-    lastToFirst someList =
-      case getLast someList of
-        Just item ->
-          item :: someList
-            |> dropLast
-        
-        Nothing ->
-            someList
-
-    shuffleJobs : List Job -> List Job
-    shuffleJobs =
-        case direction of
-          Clockwise ->
-            firstToLast
-          
-          CounterClockwise ->
-            lastToFirst
-
-    jobs =
-      record.participants
-        |> listPeople
-        |> List.map .job
-        |> shuffleJobs
-        
-  in
-  -- { record
-  --   | participants =
-  --       List.map2
-  --         ResponsiblePerson
-  --         (justThePeople record.participants)
-  --         (justTheJobs record.participants)
-  -- }
-  JobWheel record
+--rotateJobsOnce : Direction -> JobWheel -> JobWheel
+--rotateJobsOnce direction (JobWheel record) =
+--  let
+--    firstToLast : List a -> List a
+--    firstToLast someList =
+--        case List.head someList of
+--            Just item ->
+--                someList
+--                  |> (\list -> List.append list [ item ])
+--                  |> List.drop 1
+--            
+--            Nothing ->
+--                someList
+--
+--    getLast : List a -> Maybe a
+--    getLast someList =
+--        someList
+--          |> List.reverse
+--          |> List.head
+--
+--    dropLast : List a -> List a
+--    dropLast someList =
+--      let
+--          numberToTake = (List.length someList) - 1
+--      in
+--          List.take numberToTake someList
+--
+--    lastToFirst someList =
+--      case getLast someList of
+--        Just item ->
+--          item :: someList
+--            |> dropLast
+--        
+--        Nothing ->
+--            someList
+--
+--    shuffleJobs : List Job -> List Job
+--    shuffleJobs =
+--        case direction of
+--          Clockwise ->
+--            firstToLast
+--          
+--          CounterClockwise ->
+--            lastToFirst
+--
+--    jobs =
+--      record.participants
+--        |> listPeople
+--        |> List.map .job
+--        |> shuffleJobs
+--        
+--  in
+--  -- { record
+--  --   | participants =
+--  --       List.map2
+--  --         ResponsiblePerson
+--  --         (justThePeople record.participants)
+--  --         (justTheJobs record.participants)
+--  -- }
+--  JobWheel record
 
 -- in radians
 -- clockwise rotations are negative
@@ -82,7 +80,7 @@ calculateAngleOfRotation time (JobWheel record) =
         wheelTurns =
             case record.rotationDirection of
                 Clockwise ->
-                    - periods
+                    negate periods
 
                 CounterClockwise ->
                     periods
@@ -96,16 +94,155 @@ determineJobsAt time (JobWheel record) =
         jobSwitchesPerTurn =
             countPeople record.origin
 
+        theTurnDecimal =
+            turnDecimal time (JobWheel record)
+
         jobSwitches =
-             jobSwitchesPerTurn * (decimalPartOfTurns time (JobWheel record))
+             (jobSwitchesPerTurn |> toFloat) * theTurnDecimal
+
+        switchesRounded =
+            if jobSwitches >= 0 then
+                floor jobSwitches
+
+            else
+                ceiling jobSwitches
 
         personList =
-            rotateJobsNTimes jobSwitches record.origin
+            rotateJobsNTimes switchesRounded record.origin
     in
     personList
 
 
-listPeople : People -> List Person
+rotateJobsNTimes : Int -> ResponsiblePeople -> List ResponsiblePerson
+rotateJobsNTimes n responsiblePeople =
+    let
+        startingList = 
+            listPeople responsiblePeople
+
+        peopleCount =
+            List.length startingList
+
+        simplifiedN =
+            rem n peopleCount
+
+        positiveN =
+            if simplifiedN >= 0 then
+                simplifiedN
+
+            else
+                peopleCount + simplifiedN
+
+        justPeople =
+            startingList |> List.map .person
+
+        rotatedJobs =
+            startingList
+                |> List.map .job
+                |> shiftList positiveN
+    in
+    List.map2
+        ResponsiblePerson
+        justPeople
+        rotatedJobs
+
+
+shiftList : Int -> List a -> List a
+shiftList howMuch someList =
+    if howMuch < 0 then
+        shiftTowardsHead howMuch someList
+
+    else if howMuch > 0 then
+        shiftTowardsTail howMuch someList
+
+    else
+        someList
+
+
+shiftTowardsTail : Int -> List a -> List a
+shiftTowardsTail howMuch someList =
+    if howMuch < 0 then
+        shiftTowardsHead (negate howMuch) someList
+
+    else if howMuch == 0 then
+        someList
+    
+    else
+        shiftTowardsTail (howMuch - 1) (lastToFirst someList)
+
+
+shiftTowardsHead : Int -> List a -> List a
+shiftTowardsHead howMuch someList =
+    if howMuch < 0 then
+        shiftTowardsTail (negate howMuch) someList
+
+    else if howMuch == 0 then
+        someList
+    
+    else
+        shiftTowardsHead (howMuch - 1) (firstToLast someList)
+
+
+getLast : List a -> Maybe a
+getLast someList =
+    someList
+      |> List.reverse
+      |> List.head
+
+dropLast : List a -> List a
+dropLast someList =
+  let
+      numberToTake = (List.length someList) - 1
+  in
+      List.take numberToTake someList
+
+
+lastToFirst : List a -> List a
+lastToFirst someList =
+  case getLast someList of
+    Just item ->
+      item :: someList
+        |> dropLast
+        
+    Nothing ->
+        someList
+
+
+firstToLast : List a -> List a
+firstToLast someList =
+    case List.head someList of
+        Just item ->
+            someList
+              |> (\list -> List.append list [ item ])
+              |> List.drop 1
+        
+        Nothing ->
+            someList
+
+
+turnDecimal : Time.Time -> JobWheel -> Float
+turnDecimal time (JobWheel record) = 
+    let
+        turns =
+            (time - record.timeCreated) / record.period
+    in
+    if turns >= 0 then
+        getPositiveDecimal turns
+    
+    else
+        getNegativeDecimal turns
+
+
+getPositiveDecimal : Float -> Float
+getPositiveDecimal theFloat =
+    theFloat - (theFloat |> floor |> toFloat)
+        
+
+getNegativeDecimal : Float -> Float
+getNegativeDecimal theFloat =
+    theFloat - (theFloat |> ceiling |> toFloat)
+
+
+listPeople : ResponsiblePeople -> List ResponsiblePerson
 listPeople people =
     people.first :: List.append people.middle [ people.last ]
 
@@ -127,6 +264,11 @@ type alias ResponsiblePeople =
   , middle : List ResponsiblePerson
   , last : ResponsiblePerson
   }
+
+
+countPeople : ResponsiblePeople -> Int
+countPeople people =
+    1 + (List.length people.middle) + 1
 
 type alias Person =
   { id : Int
